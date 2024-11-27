b9127c3cdee939865defdcf9bc03698fb66d4e16
Xu <chenkaixusan@gmail.com>
2024-11-27 23:27:26 +0900

    refactor

--- fuse_image/fuse_image.py
+++ fuse_image/fuse_image.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-'''
+"""
 File: /workspace/temp_feedback/Temp_Feedback/fuse_image/fuse_image.py
 Project: /workspace/temp_feedback/Temp_Feedback/fuse_image
 Created Date: Monday October 7th 2024
@@ -18,52 +18,54 @@ Copyright (c) 2024 The University of Tsukuba
 HISTORY:
 Date      	By	Comments
 ----------	---	---------------------------------------------------------
-'''
-from PIL import Image
-from pathlib import Path
+"""
 
 import itertools
+from pathlib import Path
 import random
+from PIL import Image
 
-def random_scale_and_combine_two_images(base_image, overlay_image, direction='horizontal'):
-    
+
+def random_scale_and_combine_two_images(
+    base_image, overlay_image, direction="horizontal"
+):
     # 随机缩放图片
     scale_factor = random.uniform(0.3, 1.0)  # 缩放比例从0.3到1.0之间
     new_width = int(overlay_image.width * scale_factor)
     new_height = int(overlay_image.height * scale_factor)
     overlay_resized = overlay_image.resize((new_width, new_height), Image.ANTIALIAS)
-    
+
     # 生成随机位置
     max_x = base_image.width - new_width
     max_y = base_image.height - new_height
     x_position = random.randint(0, max_x)
     y_position = random.randint(0, max_y)
-    
-    # 将缩放后的图片粘贴到基础图片上
-    # FIXME: 
-    base_image.paste(overlay_resized, (x_position, y_position), overlay_resized)
 
+    # 将缩放后的图片粘贴到基础图片上
+    # FIXME:
+    base_image.paste(overlay_resized, (x_position, y_position), overlay_resized)
 
     return base_image
 
-def merge_images(cold_imgs, hot_imgs, normal_imgs, output_path, direction='horizontal'):
 
+def merge_images(cold_imgs, hot_imgs, normal_imgs, output_path, direction="horizontal"):
     # 创建组合
     combinations = itertools.product(cold_imgs + hot_imgs + normal_imgs, repeat=2)
 
     for i, (image1_path, image2_path) in enumerate(combinations):
-        
         image1 = Image.open(image1_path)
         image2 = Image.open(image2_path)
 
-        merged_img: Image = random_scale_and_combine_two_images(image1, image2, direction)
+        merged_img: Image = random_scale_and_combine_two_images(
+            image1, image2, direction
+        )
 
     # 保存合并后的图片
     merged_img.save(output_path)
-    print(f'图片已保存到 {output_path}')
+    print(f"图片已保存到 {output_path}")
+
 
 def load_images(image_path: Path):
-
     cold_imgs = []
     hot_imgs = []
     normal_imgs = []
@@ -71,21 +73,20 @@ def load_images(image_path: Path):
     for label in image_path.iterdir():
         if label.is_dir():
             for image in label.iterdir():
-                if image.suffix in ['.jpg', '.png']:
-                    if label.name == 'cold' or label.name == 'cool':
+                if image.suffix in [".jpg", ".png"]:
+                    if label.name == "cold" or label.name == "cool":
                         cold_imgs.append(image)
-                    elif label.name == 'hot' or label.name == 'warm':
+                    elif label.name == "hot" or label.name == "warm":
                         hot_imgs.append(image)
-                    elif label.name == 'normal':
+                    elif label.name == "normal":
                         normal_imgs.append(image)
     return cold_imgs, hot_imgs, normal_imgs
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     image_path = Path("/workspace/data/temp_dataset")
     output_path = Path("/workspace/data/temp_dataset/merged")
 
     cold_imgs, hot_imgs, normal_imgs = load_images(image_path)
 
-    merge_images(cold_imgs, hot_imgs, normal_imgs, output_path, direction='horizontal')
+    merge_images(cold_imgs, hot_imgs, normal_imgs, output_path, direction="horizontal")
--- project/main.py
+++ project/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-'''
+"""
 File: /workspace/temp_feedback/Temp_Feedback/project/main.py
 Project: /workspace/temp_feedback/Temp_Feedback/project
 Created Date: Thursday November 30th 2023
@@ -18,27 +18,65 @@ Copyright (c) 2023 The University of Tsukuba
 HISTORY:
 Date      	By	Comments
 ----------	---	---------------------------------------------------------
-'''
+"""
 
 import os, logging, time, sys, random
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.loggers import TensorBoardLogger
-from process import TempFeedbackLightningModule
-
-# Callbacks 
 from pytorch_lightning.callbacks import (
     ModelCheckpoint,
     EarlyStopping,
     LearningRateMonitor,
 )
 
-from dataloader.data_loader import TempDataModule
+import hydra
+import torch
 
-import hydra, torch
+from project.dataloader.data_loader import TempDataModule
+from project.trainer import TempFeedbackLightningModule
+
+
+def get_device():
+    """get device
+
+    Returns:
+        str: device name
+    """
+    if torch.cuda.is_available():  # 优先检查 CUDA
+        local_device = torch.device("cuda")
+        print(f"used device: {local_device} ({torch.cuda.get_device_name(0)})")
+    elif (
+        torch.backends.mps.is_available() and torch.backends.mps.is_built()
+    ):  # 检查 MPS
+        local_device = torch.device("mps")
+        print("used device: MPS (Metal Performance Shaders)")
+    else:  # 默认使用 CPU
+        local_device = torch.device("cpu")
+        print("used device: CPU")
+    return local_device
+
+
+@staticmethod
+def get_device_name():
+    """get device name
+
+    Returns:
+        str: device name
+    """
+    if torch.cuda.is_available():  # 优先检查 CUDA
+        device_name = "gpu"
+    elif (
+        torch.backends.mps.is_available() and torch.backends.mps.is_built()
+    ):  # 检查 MPS
+        local_device = torch.device("mps")
+        device_name = "mps"
+    else:  # 默认使用 CPU
+        local_device = torch.device("cpu")
+        device_name = "cpu"
+    return device_name
+
 
 def train(hparams):
-
     seed_everything(42, workers=True)
 
     # init datamodule
@@ -53,30 +91,28 @@ def train(hparams):
     )
 
     # init callbacks
-    lr_monitor = LearningRateMonitor(logging_interval='step')
+    lr_monitor = LearningRateMonitor(logging_interval="step")
     early_stop_callback = EarlyStopping(
-        monitor='val_loss',
-        patience=20,
-        verbose=True,
-        mode='min'
+        monitor="val_loss", patience=20, verbose=True, mode="min"
     )
     checkpoint_callback = ModelCheckpoint(
-        monitor='val_loss',
-        filename='{epoch}-{val_loss:.2f}-{val_accuracy:.2f}',
+        monitor="val_loss",
+        filename="{epoch}-{val_loss:.2f}-{val_accuracy:.2f}",
         auto_insert_metric_name=False,
         save_top_k=2,
-        mode='min',
+        mode="min",
     )
 
     # init trainer
     trainer = Trainer(
-        devices=[hparams.train.gpu_num,],
-        accelerator="gpu",
+        devices=[
+            hparams.train.gpu_num,
+        ],
+        accelerator=get_device_name,
         max_epochs=hparams.train.max_epochs,
         logger=logger,
         check_val_every_n_epoch=1,
         callbacks=[lr_monitor, early_stop_callback, checkpoint_callback],
-
     )
 
     # start training
@@ -86,12 +122,15 @@ def train(hparams):
     trainer.test(temp_model, datamodule=data_module)
 
 
-@hydra.main(config_path="/workspace/temp_feedback/Temp_Feedback/configs", config_name="config.yaml")
+@hydra.main(
+    config_path="../configs",
+    config_name="config.yaml",
+)
 def init_params(cfg):
-
+    # 获取设备
     train(cfg)
 
-if __name__ == "__main__":
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+if __name__ == "__main__":
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
     init_params()
