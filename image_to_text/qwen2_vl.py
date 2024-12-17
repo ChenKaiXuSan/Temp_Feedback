426dd4daa8e91921f940b7d4bfcd48881e3318ff
Xu <chenkaixusan@gmail.com>
2024-12-17 14:34:36 +0000

    feed different prompt into LLM and get different feed back.

--- image_to_text/qwen2_vl.py
+++ image_to_text/qwen2_vl.py
@@ -25,6 +25,8 @@ import logging
 import os 
 import json
 from PIL import Image
+import hydra
+import omegaconf
 import torch
 from typing import Dict
 from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
@@ -33,11 +35,21 @@ from pathlib import Path
 from utils.timer import timer
 from utils.get_device import get_device
 
+# 配置日志输出到文件
+# TODO: log 输出的位置需要修改一下
+logging.basicConfig(
+    filename='app.log',           # 输出的日志文件名
+    level=logging.INFO,           # 日志级别（DEBUG, INFO, WARNING, ERROR, CRITICAL）
+    format='%(asctime)s - %(levelname)s - %(message)s',  # 日志格式
+    datefmt='%Y-%m-%d %H:%M:%S'   # 日期时间格式
+)
+
 class Qwen2VL:
-    def __init__(self, output_path: Path):
+    def __init__(self, output_path: str, prompt: dict):
 
         self.device_name = get_device()
-        self.output_path = output_path
+        self.output_path = Path(output_path)
+        self.prompt = prompt
 
         self.load_model()
         self.image_info = []
@@ -77,22 +89,23 @@ class Qwen2VL:
     @timer
     def generate(self, inputs):
 
-        # Inference: Generation of the output
-        output_ids = self.model.generate(**inputs, max_new_tokens=512)
-        generated_ids = [
-            output_ids[len(input_ids) :]
-            for input_ids, output_ids in zip(inputs.input_ids, output_ids)
-        ]
-        output_text = self.processor.batch_decode(
-            generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
-        )
-        print(output_text)
+        with torch.inference_mode():
+            # Inference: Generation of the output
+            output_ids = self.model.generate(**inputs, max_new_tokens=512)
+            generated_ids = [
+                output_ids[len(input_ids) :]
+                for input_ids, output_ids in zip(inputs.input_ids, output_ids)
+            ]
+            output_text = self.processor.batch_decode(
+                generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            logging.info(output_text)
 
         return output_text
     
     def save_image_info_to_json(self, image_info: dict, json_file_path: Path):
         
-        full_path = Path(os.getcwd()) / json_file_path
+        full_path = json_file_path
 
         if full_path.exists() is False:
             full_path.parent.mkdir(parents=True, exist_ok=True)
@@ -102,50 +115,54 @@ class Qwen2VL:
 
     def __call__(self, image_path: Path, text: str = "Describe this image."):
 
-        image = Image.open(str(Path(os.getcwd()) / image_path)).convert("RGB")
-        print(image.size)
+        image = Image.open(image_path).convert("RGB")
+        logging.info(image.size)
 
-        conversation = [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image",
-                    },
-                    {"type": "text", "text": text},
-                ],
-            }
-        ]
+        whole_output_text = {}
 
-        inputs = self.preprocess(conversation, image, self.device_name)
+        for idx, text in self.prompt.items():
+            conversation = [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image",
+                        },
+                        {"type": "text", "text": text},
+                    ],
+                }
+            ]
 
-        output_text = self.generate(inputs)        
+            inputs = self.preprocess(conversation, image, self.device_name)
+
+            whole_output_text[idx] = self.generate(inputs)        
         
         # package the image info
         image_info = {
             "image_name": image_path.stem,
             "image_path": str(image_path),
             "conversation": conversation,
-            "output_text": output_text,
+            "output_text": whole_output_text,
         }
 
         self.image_info.append(image_info)
 
         self.save_image_info_to_json(self.image_info, self.output_path / "image_info.json")
             
-
-
-if __name__ == "__main__":
-
-    output_path = Path("logs/qwen2-vl_result")
+@hydra.main(config_path="../configs", config_name="gpt_prompts")
+def load_config(cfg: omegaconf.DictConfig):
+    
+    output_path = cfg.output_path
 
     # test image path
     images_path = [Path("tests/data/sample.jpg"), Path("tests/data/fire.jpg")]
-    
-    text: str = "Describe the tempurature of the figure."
-    image_to_text = Qwen2VL(output_path=output_path)
 
+    image_to_text = Qwen2VL(output_path=output_path, prompt=cfg.prompt)
 
     for image_path in images_path:
-        image_to_text(image_path, text)
+        image_to_text(image_path)
     
+    
+if __name__ == "__main__":
+
+    load_config()
